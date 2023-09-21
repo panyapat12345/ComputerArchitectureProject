a790e4dc94c567221f588e655fb811bcf5f99a72
JEXB <jippee.1211@gmail.com>
2023-09-21 22:18:21 +0700

    readFile

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            String fileName = "test3.txt";  // Replace with the actual file path
+            String program = ReadFile.readFile(fileName);
+
+            ExprTokenizer tokenizer = new ExprTokenizer(program);
+
+            while (tokenizer.hasNextToken()) {
+                String token = tokenizer.consume();
+                String[] tokens = token.trim().split("\\s+");
+                for (String t : tokens) {
+                    if (!t.isEmpty()) {
+                        System.out.println("Token: " + t);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LexicalError | SyntaxError e) {
+            System.err.print("Lexical or Syntax Error: " + e.getMessage());
+        }
     }
 }
