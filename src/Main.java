a85071b0655fb1a6cf37176cd8853376031ac6cd
Naihtton <whitediary1@outlook.com>
2023-10-13 17:17:09 +0700

    use this combination.txt instead

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import Exceptions.Exit;
 public class Main {
     public static void main(String[] argv) {
         try {
-            Assembler.getMachineCodes("src/testCases/test_1.txt", "src/testCases/Assemblyoutput.txt");
-//            Assemble.getMachineCodes("src/testCases/Fibonucci.txt", "src/testCases/Assemblyoutput.txt");
-//            Assemble.getMachineCodes("src/testCases/multiplytest.txt", "src/testCases/Assemblyoutput.txt");
-//            Assemble.getMachineCodes("src/testCases/testcombination.txt", "src/testCases/Assemblyoutput.txt");
-//            Assemble.getMachineCodes("src/testCases/seriestest.txt", "src/testCases/Assemblyoutput.txt");
+//            Assembler.getMachineCodes("src/testCases/test_1.txt", "src/testCases/Assemblyoutput.txt");
+//            Assembler.getMachineCodes("src/testCases/Fibonucci.txt", "src/testCases/Assemblyoutput.txt");
+//            Assembler.getMachineCodes("src/testCases/multiplytest.txt", "src/testCases/Assemblyoutput.txt");
+              Assembler.getMachineCodes("src/testCases/Combination.txt", "src/testCases/Assemblyoutput.txt");
+//            Assembler.getMachineCodes("src/testCases/seriestest.txt", "src/testCases/Assemblyoutput.txt");
 
             System.out.println();
             System.out.println();
